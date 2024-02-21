c4df7bc530b0216d607cc9346bfeb3ea23718d8c
kooyesung <jh8ckr@gmail.com>
2024-02-21 10:09:55 +0900

    Calculator-print initial commit

--- src/main/java/com/ohgiraffers/calculator/Calculator.java
+++ src/main/java/com/ohgiraffers/calculator/Calculator.java
@@ -1,4 +1,10 @@
 package com.ohgiraffers.calculator;
 
 public class Calculator {
+
+
+    public void print (int[] arrNum, String calSybol, int total ) {
+        System.out.println("계산중...");
+        System.out.println(arrNum[0] + " " + calSybol + " " + arrNum[1] + " = " + total);
+    }
 }
