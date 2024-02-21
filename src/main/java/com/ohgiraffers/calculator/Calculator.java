03d9ae85c6d4c0b92d43d8575379cdaf2a41cfc4
Java_Literal <zziny725@icloud.com>
2024-02-21 10:23:36 +0900

    Calculator-calculator initial commit

--- src/main/java/com/ohgiraffers/calculator/Calculator.java
+++ src/main/java/com/ohgiraffers/calculator/Calculator.java
@@ -1,4 +1,62 @@
 package com.ohgiraffers.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+    Scanner sc = new Scanner(System.in);
+
+
+    public int calculator(int[] arrNum, char symbol) {
+
+        int result = 0;
+
+        switch (symbol) {
+
+            case '+' :
+                result = arrNum[0] + arrNum[1];
+                break;
+
+            case '-' :
+                result = arrNum[0] - arrNum[1];
+                break;
+
+            case '*' :
+                result = arrNum[0] * arrNum[1];
+                break;
+
+            case '/' :
+                result = arrNum[0] / arrNum[1];
+
+//                if (arrNum[1] == 0) {
+
+
+
+//                }
+                break;
+
+            case '%' :
+                result = arrNum[0] + arrNum[1];
+                break;
+        }
+
+            return result;
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
