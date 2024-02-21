6db3f44e91ad73e4850c017a2f32eb9ab30cf6d3
minseo <juhee0629032@gmail.com>
2024-02-21 10:00:28 +0900

    Calculator-inputNumber initial commit

--- src/main/java/com/ohgiraffers/calculator/Calculator.java
+++ src/main/java/com/ohgiraffers/calculator/Calculator.java
@@ -1,4 +1,17 @@
 package com.ohgiraffers.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+    public int[] inputNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("계산하실 정수 두 개를 입력하세요. ");
+        System.out.print("첫 번째 정수 : ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 정수 : ");
+        int num2 = sc.nextInt();
+        int[] arrNum = {num1, num2};
+        return arrNum;
+    }
 }
