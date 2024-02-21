f25942513d3905bce66354a1e1c938de1601cc4b
sunskwon <sunskwon@gmail.com>
2024-02-21 09:56:11 +0900

    feature/inputSymbol

--- src/main/java/com/ohgiraffers/calculator/Calculator.java
+++ src/main/java/com/ohgiraffers/calculator/Calculator.java
@@ -1,4 +1,21 @@
 package com.ohgiraffers.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+    public String inputSymbol() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("사칙연산 기호를 입력하세요 ('+', '-', '*', '/', '%'");
+        String calSymbol = sc.next();
+
+        if (!(calSymbol == "+" || calSymbol == "-" || calSymbol == "*" || calSymbol == "/" || calSymbol == "%")) {
+
+            System.out.println("잘못된 기호를 입력하셨습니다.");
+        }
+
+        return calSymbol;
+    }
+
 }
